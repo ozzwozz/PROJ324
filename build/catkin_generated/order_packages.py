72df801ded3c6bb42119bb6213dc4ba3b6bd858c
Oscar <oscardawe@googlemail.com>
2021-02-22 18:46:13 +0000

    For Reddit

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = '/home/oscar/Documents/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
+whitelisted_packages = 'image_processing'.split(';') if 'image_processing' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
 underlay_workspaces = '/home/oscar/Documents/catkin_ws/devel;/opt/ros/kinetic'.split(';') if '/home/oscar/Documents/catkin_ws/devel;/opt/ros/kinetic' != '' else []
