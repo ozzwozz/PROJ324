4bba1ca03d38b1d4869020b96705d2efe422da30
Oscar <oscardawe@googlemail.com>
2021-03-01 21:34:37 +0000

    Target tracker node , successfully subscribes to /camera/rgb/image_raw topic

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -9,3 +9,5 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/oscar/Documents/catkin_ws/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/oscar/Documents/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/oscar/Documents/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/oscar/Documents/catkin_ws/src:$ROS_PACKAGE_PATH"
