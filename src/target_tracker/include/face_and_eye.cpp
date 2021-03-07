c65a92d1f47f766e93681c5f63456699474ce8dd
Oscar <oscardawe@googlemail.com>
2021-03-07 16:34:26 +0000

    added cascade training

---
+++ src/target_tracker/include/face_and_eye.cpp
@@ -0,0 +1,9 @@
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+class face_and_eye
+{
+
+};
