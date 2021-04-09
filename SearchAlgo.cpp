c83e5b7547b566fe00ce564fcaf3851bf3a4ed7b
ozzwozz <61410630+ozzwozz@users.noreply.github.com>
2021-04-09 21:28:10 +0100

    Room search class started

---
+++ SearchAlgo.cpp
@@ -0,0 +1,14 @@
+void SearchingMappedArea::ConvertMapToGraph()
+{
+	int mapwidth;
+	int mapheight;
+	int GraphMatrix[mapwidth][mapheight];
+	for (int x = 0; x < mapwidth; x++)
+	{
+		for (int y =0; y < mapheight; y++)
+		{
+			if (GraphMatrix[x][y] == '0')
+			GraphMatrix[x][y] = ;
+		}
+	}
+}
---
+++ SearchAlgo.hpp
@@ -0,0 +1,12 @@
+class SearchingMappedArea 
+{
+	private:
+
+	protected:
+		void ConvertMapToGraph();
+
+	public:
+		
+
+
+};
