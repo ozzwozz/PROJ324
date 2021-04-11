f1b248bc65e905445c46122dee2386715b0a4685
Oscar <oscardawe@googlemail.com>
2021-04-11 19:14:33 +0100

    Summit LaserScan topic displays in rviz. HOWEVER map is displayed off-centered

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,10 @@
 # new environment variables
 
 # modified environment variables
-export LD_LIBRARY_PATH='/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu'
-export PKG_CONFIG_PATH='/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig'
+export CMAKE_PREFIX_PATH="/home/oscar/Documents/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/oscar/Documents/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/oscar/Documents/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/oscar/Documents/catkin_ws/build'
-export PYTHONPATH='/opt/ros/kinetic/lib/python2.7/dist-packages'
+export PYTHONPATH="/home/oscar/Documents/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/oscar/Documents/catkin_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/oscar/Documents/catkin_ws/src:$ROS_PACKAGE_PATH"
