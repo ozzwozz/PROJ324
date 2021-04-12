e2ea0e8bda83f00e3d121e161a4ef7303c29eb0f
Oscar <oscardawe@googlemail.com>
2021-04-12 21:04:45 +0100

    move_base fixed (path planning), it works outright when 2D nav goal is given

---
+++ build/confined_environment_search/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/oscar/Documents/catkin_ws/src/confined_environment_search
+# Build directory: /home/oscar/Documents/catkin_ws/build/confined_environment_search
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
