58127829706d8f4d08408eef6e2b5e7e3daddf87
Oscar <61410630+ozzwozz@users.noreply.github.com>
2021-04-23 14:09:07 +0100

    action server made for the confined_environment_search

--- src/confined_environment_search/include/SearchAlgo.hpp
+++ src/confined_environment_search/include/SearchAlgo.hpp
@@ -1,3 +1,7 @@
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+
 class SearchingMappedArea
 {
 	ros::NodeHandle nh_;
@@ -7,9 +11,15 @@ class SearchingMappedArea
 	ros::Publisher SearchGoal;
 	ros::Publisher cameraPoint_pub_;
 
-	SearchGoal;
+    
+    typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+    
+    move_base_msgs::MoveBaseGoal SearchGoal;
+    
 	private:
 		void SearchCallback();
+    void CollateTargetPose(int target_x_position, int target_w_orientation);
+
 	protected:
 		void ConvertMapToGraph();
 
@@ -19,6 +29,14 @@ class SearchingMappedArea
 			PoseSub_ = nh_.subscribe("/odom", 1, &SearchingMappedArea::SearchCallback, this);
 
 			SearchGoal = nh_.advertise("/move_base_simple/goal", 1);
+            
+            //tell the action client that we want to spin a thread by default
+            MoveBaseClient ac("move_base", true);
+            
+            //wait for the action server to come up
+            while(!ac.waitForServer(ros::Duration(5.0))){
+              ROS_INFO("Waiting for the move_base action server to come up");
+            }
 		}
 		~SearchingMappedArea()
 		{
--- src/confined_environment_search/src/SearchAlgo.cpp
+++ src/confined_environment_search/src/SearchAlgo.cpp
@@ -16,18 +16,58 @@ void SearchingMappedArea::ConvertMapToGraph()
 
 void SearchingMappedArea::SearchCallback(const nav_msgs::Odometry& msg)
 {
-
-
+    CalculateNextPose();
+    CollateTargetPose(target_x_position, target_w_orientation);
+    GoalStateCheck();
 
 }
+
+void SearchingMappedArea::CalculateNextPose()
+{
+    
+}
+
+void SearchingMappedArea::GoalStateCheck()
+{
+    ROS_INFO("Sending goal");
+    ac.sendGoal(SearchGoal);
+
+    ac.waitForResult();
+
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    {
+      ROS_INFO("Hooray, the base moved 1 meter forward");
+    }
+    else if(ac.getState() == actionlib::SimpleClientGoalState::ABORTED)
+    {
+        ROS_INFO("Move has been aborted due to target aquisition");
+    }
+    else
+    {
+        ROS_INFO("The base failed to move forward 1 meter for some reason");
+    }
+}
+
+void SearchingMappedArea::CollateTargetPose(int target_x_position, int target_w_orientation)
+{
+    //we'll send a goal to the robot to move 1 meter forward
+    SearchGoal.target_pose.header.frame_id = "summit_xl_a_base_footprint";
+    SearchGoal.target_pose.header.stamp = ros::Time::now();
+
+    SearchGoal.target_pose.pose.position.x = target_x_position;
+    SearchGoal.target_pose.pose.orientation.w = target_w_orientation;
+}
+
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "area_search");
-  SearchingMappedArea SearchMappedArea;
-  while(ros::ok())
-  {
-    ros::spinOnce();
-  }
-  return 0;
+    ros::init(argc, argv, "area_search");
+    SearchingMappedArea SearchMappedArea;
+    
+    while(ros::ok())
+    {
+        ros::spinOnce();
+    }
+    return 0;
 }
+
