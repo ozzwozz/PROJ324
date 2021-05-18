32fde1530ad6aac053915f33bbf010f96fc3afa4
Oscar <61410630+ozzwozz@users.noreply.github.com>
2021-05-18 16:10:40 +0100

    test file added

---
+++ testing.py
@@ -0,0 +1,23 @@
+
+
+def main():
+    DataFile = open("target_data.csv", "r")
+
+    for x in DataFile:
+        # where line[0] is actual location and line[1] is predicted
+        Line = x.split("#")
+        Actual_Loc = Line[0].split(",") # [0] is x, [1] is y
+        Predictied_Loc = Line[1].split(",")# [0] is x, [1] is y
+
+        x_accuracy = (Actual_Loc[0]/Predictied_Loc[0]) * 100
+        y_accuracy = (Actual_Loc[1]/Predictied_Loc[1]) * 100
+
+
+        print("Location",x,"x_accuracy",x_accuracy,"y_accuracy", y_accuracy)
+        DataFile.close()
+
+
+
+# calls main function, standard practice
+if __name__ == "__main__":
+    main()
